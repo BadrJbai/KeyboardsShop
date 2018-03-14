494397700da4ce8daac7b24c8e24eff18e1dd5db
Badrjbai <badrjbai@gmail.com>
2018-03-14 16:16:01 -0400

    fooling with property bidings and dynamic calls/events with buttons

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {setTimeout} from "timers";
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer =false;
+  constructor() {
+      setTimeout(() => { this.allowNewServer =true;}, 2000);
+  }
 
   ngOnInit() {
   }
